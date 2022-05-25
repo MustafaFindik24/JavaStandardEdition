8f718a477a3309362609a64435e3bcb84d4b8a5c
MustafaFındık <91599453+MustafaFindik24@users.noreply.github.com>
2022-05-26 00:37:39 +0300

    practicing in java

---
+++ src/JavaIntroduction/Main.java
@@ -0,0 +1,16 @@
+package JavaIntroduction;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        float a = 32.3F;
+        byte b = 127;
+        if(b>a){
+            System.out.println(b + " is greater than " + a);
+        }
+        else{
+            System.out.println(a + " is greater than " + b);
+        }
+    }
+}
---
+++ src/JavaOOP/Main.java
@@ -0,0 +1,7 @@
+package JavaOOP;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
