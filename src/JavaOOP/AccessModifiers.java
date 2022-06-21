d3003dc0faeafc6d42c265aa2d097b94e71b9932
MustafaFındık <91599453+MustafaFindik24@users.noreply.github.com>
2022-06-21 13:22:06 +0300

    java and inheritance classes added

--- src/JavaOOP/AccessModifiers.java
+++ src/JavaOOP/AccessModifiers.java
@@ -7,12 +7,25 @@ class AccessModifiers {
         System.out.println(this);
     }
     public static void main(String[] args) {
+        String hello = "hi";
+        int world = 2;
         Main main = new Main();
+        boolean result1 = main instanceof Main;          //instanceof using
+        boolean result2 = hello instanceof String;
+        System.out.println(result1 + " "+ result2);
         main.setName("hello");
         System.out.println(main.getName());
         System.out.println("------------------------------");
         AccessModifiers acm = new AccessModifiers(3);       //this and acm object are same reference
         System.out.println(acm);
-
+        System.out.println("------------------------------");
+        int result = factorial(4);
+        System.out.println(result);
+    }
+    static int factorial(int n){
+        if(n==0)
+             return 1;
+        else
+             return  n * factorial(n - 1);
     }
 }
---
+++ src/JavaOOP/Java.java
@@ -0,0 +1,6 @@
+package JavaOOP;
+
+public class Java {
+
+}
+
