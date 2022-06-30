ca2c2c0a9c4d8b7a8de1f22614d09bf141f00a8e
MustafaFındık <91599453+MustafaFindik24@users.noreply.github.com>
2022-06-30 17:52:22 +0300

    functional interface added

---
+++ src/AdditionalTopics/Functional.java
@@ -0,0 +1,5 @@
+package AdditionalTopics;
+@FunctionalInterface
+public interface Functional {
+    String write();
+}
--- src/AdditionalTopics/Lambda.java
+++ src/AdditionalTopics/Lambda.java
@@ -1,7 +1,37 @@
 package AdditionalTopics;
 
-public class Lambda {
-    public static void main(String[] args) {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
+public class Lambda {
+    static List<String> places = new ArrayList<>();
+
+    public static List getPlaces(){
+        places.add("Nepal, Kathmandu");
+        places.add("Nepal, Pokhara");
+        places.add("India, Delhi");
+        places.add("USA, New York");
+        places.add("Africa, Nigeria");
+
+        return places;
+
+    }
+    public static void main(String[] args) {
+        //Functional a = () -> 2;
+        //System.out.println("Number is " + a.getNum());
+        System.out.println("---------------------------");
+        List<String> myPlace = getPlaces();
+
+            myPlace.stream()
+                    .filter((p) -> p.startsWith("Nepal"))
+                    //.map((p) -> p.toUpperCase())
+                    .sorted()
+                    .forEach((p) -> System.out.println(p));
+
+            Functional m = () -> {
+            return "Hello world";
+            };
+        System.out.println(m.write());
     }
 }
